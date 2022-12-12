7c0f4db3c8f6dff8cab8e570b8a791d7056329aa
Chris Marron <chrisluk04@gmail.com>
2022-12-12 21:17:09 +0000

    Completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 10;
+var height = 11;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 100;
+var payment = 120;
+var change = bill - payment;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 4;
+var midterm = 4;
+var final = 6;
+var grade = quizzes + midterm + final;
+console.log('value of grade:', grade / 3);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Chris';
+var lastName = 'Marron';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 5;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 7;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('log the value of:', motto);
+console.log('typeof motto:', typeof motto);
