f92841ab981fef04b1ee87df19993b1e9554369b
Chris Marron <chrisluk04@gmail.com>
2022-12-14 00:15:36 +0000

    Completed javascript-functions-practice

---
+++ javascript-functions-practice/get-area-of-circle.js
@@ -0,0 +1,5 @@
+/* exported getAreaOfCircle */
+function getAreaOfCircle(radius) {
+  var a = Math.PI * radius * radius;
+  return a;
+}
---
+++ javascript-functions-practice/get-right-triangle-area.js
@@ -0,0 +1,5 @@
+/* exported getRightTriangleArea */
+function getRightTriangleArea(base, height) {
+  var sum = base * height;
+  return sum / 2;
+}
