441c406a5edc3982c497f3b089bf8cd50b0ed28e
Chris Marron <chrisluk04@gmail.com>
2023-01-02 23:59:58 +0000

    added whitespace to make it look better.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -7,17 +7,17 @@ function characterHandler(e) {
     start = 0;
     test[0].className = 'start current';
     for (var i = 1; i < test.length; i++) {
-      test[i].className = '';
+      test[i].className = 'whitespace';
     }
   }
   if (e.key === test[start].textContent) {
     if (start < test.length) {
       test[start].className += ' pressed';
       if (test[start].className.includes('start')) {
-        test[start].className = 'pressed ';
+        test[start].className = 'pressed whitespace';
       }
       start++;
-      test[start].className = 'start';
+      test[start].className = 'start whitespace';
 
     }
   } else {
