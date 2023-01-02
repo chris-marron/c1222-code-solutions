c655c4d611b3f1af7eb96d931c8f2966a0e1e48a
Chris Marron <chrisluk04@gmail.com>
2023-01-02 23:15:01 +0000

    reset all js

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,10 +1,27 @@
 var test = document.querySelectorAll('span');
+var start = 0;
 
-for (var i = 0; i < test.length; i++) {
-  var oneTest = test[i].textContent;
-  document.addEventListener('keydown', function (e) {
-    if (e.key === 'i') {
-      oneTest.className = 'pressed';
+function characterHandler(e) {
+  if (start === test.length) {
+    window.alert('test');
+    start = 0;
+    test[0].className = 'start current';
+    for (var i = 1; i < test.length; i++) {
+      test[i].className = 'whitespace';
     }
-  });
+  }
+  if (e.key === test[start].textContent) {
+    if (start < test.length) {
+      test[start].className += ' pressed';
+      if (test[start].className.includes('start')) {
+        test[start].className = 'pressed whitespace';
+      }
+      start++;
+      test[start].className = 'start whitespace';
+    }
+  } else {
+    test[start].className += ' wrong-key';
+  }
 }
+
+document.addEventListener('keydown', characterHandler);
