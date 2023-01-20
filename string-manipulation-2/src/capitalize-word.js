18d22119fadf51d6f4c9a404c6f70274fa81edcf
Chris Marron <chrisluk04@gmail.com>
2023-01-20 22:09:03 +0000

    changed all the mumbo to return JavaScript

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -3,7 +3,7 @@ function capitalizeWord(word) {
   var newWord = word.toLowerCase();
   newWord = newWord.slice(0, 1).toUpperCase() + newWord.slice(1);
   if (newWord === 'Javascript') {
-    newWord = newWord.slice(0, 1).toUpperCase() + newWord.slice(1, 4) + newWord.slice(4, 5).toUpperCase() + newWord.slice(5);
+    return 'JavaScript';
   }
   return newWord;
 }
