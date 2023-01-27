4674a398724caf349f33ce3d0401597418bea7ac
Chris Marron <chrisluk04@gmail.com>
2023-01-27 19:52:39 +0000

    Completed express-delete

---
+++ express-delete/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  const arr = [];
+  for (const id in grades) {
+    arr.push(grades[id]);
+  }
+  res.json(arr);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  for (const nums in grades) {
+    if (req.params.id === nums) {
+      delete grades[nums];
+      res.sendStatus(204);
+    }
+  }
+
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000!');
+});
