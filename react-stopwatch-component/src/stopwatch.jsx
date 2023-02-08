9fc18d0e58949952d18fa6ffcdbb7daa0fc1d261
Chris Marron <chrisluk04@gmail.com>
2023-02-08 00:43:40 +0000

    changed it to a singular fontawesome, unset interval

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -1,42 +1,43 @@
 import React, { useState } from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import { faCircle } from '@fortawesome/free-regular-svg-icons';
+
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
+
 export default function Stopwatch({ into }) {
   const [begin, setBegin] = useState(0);
   const [timerOn, setTime] = useState(false);
   const [clearIntervalID, setIntervalID] = useState();
 
   function onStart() {
-    setTime(true);
-    const interval = setInterval(() => {
-      setBegin(begin => begin + 1);
-    }, 1000);
-    setIntervalID(interval);
+    if (timerOn) {
+      clearInterval(clearIntervalID);
+      setTime(false);
+      setIntervalID(undefined);
+
+    } else {
+      setTime(true);
+      const interval = setInterval(() => {
+        setBegin(begin => begin + 1);
+      }, 1000);
+      setIntervalID(interval);
+    }
   }
 
-  function handlePause() {
-    clearInterval(clearIntervalID);
-    setTime(false);
-    setIntervalID(undefined);
-  }
   function resetOnPause() {
     if (!timerOn) return setBegin(0);
   }
 
-  // const isPlaying = !timerOn ? faPlay : faPause;
+  const isPlaying = !timerOn ? faPlay : faPause;
 
   return (
     <>
       <div className='play-on'>
         <h1 className='num'>{begin}</h1>
         <FontAwesomeIcon icon={faCircle} className="circle" onClick={resetOnPause} />
-
-        {!timerOn
-          ? <FontAwesomeIcon onClick={onStart} icon={faPlay} className='play'/>
-          : (
-            <FontAwesomeIcon icon={faPause} onClick={handlePause} className='pause'/>)
-        }
+        <FontAwesomeIcon onClick={onStart} icon={isPlaying} className='play'/>
       </div>
     </>
   );
