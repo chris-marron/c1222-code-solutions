d406bf7f75138053713fc04a98e60ec34419ff66
Chris Marron <chrisluk04@gmail.com>
2023-02-08 00:21:09 +0000

    completed react-validated-input-component

---
+++ react-validated-input-component/src/validated-input.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+export default function ValidatedInput() {
+  const [isPassword, setIsPassword] = useState('');
+  const [tooShort, setTooShort] = useState(false);
+
+  function isEight(e) {
+    setIsPassword(e.target.value);
+    if (e.target.value === '') {
+      setTooShort(false);
+    }
+    if (e.target.value.length <= 8) {
+      setTooShort(false);
+    } else {
+      (
+        setTooShort(true)
+      );
+    }
+
+  }
+  const test = !tooShort ? 'xmark' : 'check';
+
+  return (
+    <div className='row'>
+      <form action="">
+        <h1 className='pass'>Password</h1>
+        <label htmlFor="">
+          <input name='password' type="password" value={isPassword} required onChange={isEight} className='bar'/>
+          <i className={`fa-solid fa-${test}`}></i>
+          {isPassword === '' ? <p className='error'>A password is required.</p> : ''}
+          {isPassword.length <= 8 && isPassword !== '' ? <p className='error'>Your password is too short</p> : ''}
+        </label>
+      </form>
+    </div>
+  );
+}
