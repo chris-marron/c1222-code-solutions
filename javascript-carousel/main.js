3ab7ef198301540388378d5010285588f3af4985
Chris Marron <chrisluk04@gmail.com>
2023-02-11 23:01:43 +0000

    in-prog

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -22,6 +22,7 @@ const images = [
 ];
 
 const dots = document.querySelector('#ok');
+const img = document.querySelector('#poke-img');
 
 function domCr(es) {
   const dok = document.createElement('i');
@@ -35,12 +36,46 @@ function loops() {
   for (let i = 0; i < images.length; i++) {
     domCr(images[i].src);
   }
-  const dotse = document.querySelectorAll('i');
+  const dotse = document.querySelectorAll('.fa-circle');
   for (let j = 0; j < dotse.length; j++) {
     dotse[j].addEventListener('click', e => {
-      // console.log(e);
+      img.setAttribute('src', images[j].src);
+
+      if (dotse[j]) {
+        dotse[j].classList.add('dark');
+      } else {
+        dotse[j].classList.remove('dark');
+      }
     });
   }
+  clearInterval(interval);
+  interval = setInterval(next, 3000);
 }
+const right = document.querySelector('.fa-angle-right');
+const left = document.querySelector('.fa-angle-left');
+left.addEventListener('click', () => {
+  if (num === 0) {
+    num = images.length - 1;
+  } else {
+    num--;
+  }
+  clearInterval(interval);
+  interval = setInterval(next, 3000);
+  return img.setAttribute('src', images[num].src);
 
+});
+let num = 0;
+function next() {
+  if (num === images.length - 1) {
+    num = 0;
+  } else {
+    num++;
+  }
+  clearInterval(interval);
+  interval = setInterval(next, 3000);
+  return img.setAttribute('src', images[num].src);
+}
+right.addEventListener('click', next);
+
+let interval = setInterval(next, 3000);
 loops();
