3ce4e92dc9890ab35c32767c3b5aa92db3ff3ff3
Chris Marron <chrisluk04@gmail.com>
2023-02-13 19:18:59 +0000

    completed fetch

---
+++ fetch/main.js
@@ -0,0 +1,12 @@
+fetch('https://jsonplaceholder.typicode.com/users', {
+  method: 'GET'
+})
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+fetch('https://pokeapi.co/api/v2/pokemon/tyranitar', {
+  method: 'GET'
+
+})
+  .then(response => response.json())
+  .then(data => console.log(data));
