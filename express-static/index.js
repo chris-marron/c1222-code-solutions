2cbcdc962387d9b7a8d50b0cd243fcf1cffdc2c4
Chris Marron <chrisluk04@gmail.com>
2023-02-13 03:12:49 +0000

    completed express-static

--- express-static/index.js
+++ express-static/index.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require('path');
 const port = 3000;
 const publica = path.join(__dirname, 'public');
+console.log('path:', publica);
 const statica = express.static(publica);
+console.log('express static', statica);
 app.use(statica);
 
 app.listen(port, () => {
